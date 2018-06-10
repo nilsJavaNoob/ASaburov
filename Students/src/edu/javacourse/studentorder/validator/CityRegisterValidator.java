2c5511d9b9d8a88310cc639ba9e872fd96cb8d85
nilsJavaNoob <nils1968@gmail.com>
2018-06-10 11:25:54 +0300

    vipusk 20 polymorphism interfaces   06.06. 18

--- Students/src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
+++ Students/src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
@@ -1,7 +1,9 @@
 package edu.javacourse.studentorder.validator;
 
 import edu.javacourse.studentorder.domain.AnswerCityRegister;
+import edu.javacourse.studentorder.domain.CityRegisterCheckerResponse;
 import edu.javacourse.studentorder.domain.StudentOrder;
+import edu.javacourse.studentorder.exception.CityRegisterException;
 
 /**
  * Created by n on 12.05.18.
@@ -10,13 +12,28 @@ public class CityRegisterValidator {
 
     public String hostName;
     protected int port;
+    private String login;
     String password;
-    String login;
-    //это пока заглушка
+    private CityRegisterChecker personChecker;//interface
+
+    public CityRegisterValidator() {
+
+        personChecker = new RealCityRegisterChecker();
+    }
+
+
+    //
      public AnswerCityRegister checkCityRegister(StudentOrder so){
-        System.out.println("CityRegister is running:" + hostName + " " + login + " " + password);
+        try{
+            CityRegisterCheckerResponse hans = personChecker.checkPerson(so.getHusband());
+            personChecker.checkPerson(so.getWife());
+            personChecker.checkPerson(so.getChild());
+        } catch (CityRegisterException ex){
+            ex.printStackTrace();
+        }
+
+
         AnswerCityRegister ans = new AnswerCityRegister();
-        ans.success = false;
         return  ans;
     }
-}
+}//class
